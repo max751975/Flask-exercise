088089674dffa1f539aaaf8cad39bbaa80447bc4
Maxim Curchin <max751975@gmail.com>
2022-11-27 23:42:31 -0600

    completed Flask exercise

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,53 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+
+@app.route('/add')
+
+def addition():
+    '''Returns the sum of 2 numbers'''
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(add(a, b))
+
+@app.route('/sub')
+    
+def subtract():
+    """Subtracts b from a"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(sub(a, b))
+
+@app.route('/mult')
+    
+def multiplication():
+    """Multiplies a and b"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(mult(a, b))
+
+@app.route('/div')
+    
+def division():
+    """Divides a by b"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(div(a, b))
+
+MATH = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div
+}
+ @app.route('/math/<math_op>')
+ def do_math(math_op):
+    """Do math on a and b"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = MATH[math_op](a, b)
+    return result
--- greet/app.py
+++ greet/app.py
@@ -14,3 +14,42 @@ def home_page():
         >The Home Page</h1>
         '''
     return html
+
+@app.route('/welcome')
+def welcome():
+    html = '''
+        <h1 
+        style = "
+        font-size:3em; 
+        color:purple; 
+        text-decoration: underline 3px purple;
+        text-align: center"
+        >Welcome!!!</h1>
+        '''
+    return html
+@app.route('/welcome/home')
+def welcome_home():
+    html = '''
+        <h1 
+        style = "
+        font-size:3em;
+        margin:5em auto; 
+        color:purple; 
+        text-decoration: underline 3px purple;
+        text-align: center"
+        >Welcome Home!!!</h1>
+        '''
+    return html
+@app.route('/welcome/back')
+def welcome_back():
+    html = '''
+        <h1 
+        style = "
+        font-size:3em;
+        margin: 5em auto; 
+        color:purple; 
+        text-decoration: underline 3px purple;
+        text-align: center"
+        >Welcome Back!!!</h1>
+        '''
+    return html
